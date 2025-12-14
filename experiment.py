4bc452a584e7478cb01cb7f48becca9efdeabf17
sofiiakapsha <sofiia.kapsha.25@kse.org.ua>
2025-12-14 13:59:59 +0200

    experiment.py

--- experiment.py
+++ experiment.py
@@ -6,22 +6,25 @@ def run_experiment(
         sizes=None,
         densities=None,
         trials=20,
-        output_file="results.txt"
+        output_file="results.txt",
+        avg_output_file="results_avg.txt"
 ):
     if sizes is None:
         sizes = list(range(20, 201, 20))
     if densities is None:
         densities = [10, 30, 50, 70, 90]
 
-    results = []
     total = len(sizes) * len(densities) * trials
     run_id = 0
 
-    with open(output_file, "w") as f:
-        f.write("n\tdensity\ttrial\ttime_sec\n")
+    with open(output_file, "w") as f_all, open(avg_output_file, "w") as f_avg:
+        f_all.write("n\tdensity\ttrial\ttime_sec\n")
+        f_avg.write("n\tdensity\tavg_time_sec\n")
 
         for density in densities:
             for n in sizes:
+                times = []
+
                 for t in range(trials):
                     run_id += 1
                     print(f"[{run_id}/{total}] n={n}, density={density}%, trial={t+1}")
@@ -33,19 +36,16 @@ def run_experiment(
                     end = time.perf_counter()
 
                     elapsed = end - start
-                    print(f"   час виконання = {elapsed:.6f} сек")
+                    times.append(elapsed)
 
-                    f.write(f"{n}\t{density}\t{t+1}\t{elapsed:.6f}\n")
+                    f_all.write(f"{n}\t{density}\t{t+1}\t{elapsed:.6f}\n")
 
-                    results.append({
-                        "n": n,
-                        "density": density,
-                        "trial": t + 1,
-                        "time_sec": elapsed
-                    })
+                avg_time = sum(times) / len(times)
+                f_avg.write(f"{n}\t{density}\t{avg_time:.6f}\n")
+
+                print(f"   СЕРЕДНІЙ ЧАС = {avg_time:.6f} сек\n")
 
     print("\nЕксперимент завершено!")
-    return results
 
 if __name__ == "__main__":
-    res = run_experiment()
+    run_experiment()
